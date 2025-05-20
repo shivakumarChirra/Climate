ab195a64704e6e51a117224e832d6802550fed5b
shivakumarchirra <chirrashivakumar5@gmail.com>
2025-05-20 14:27:57 +0530

    Initial Commit

---
+++ Climate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Climate
+//
+//  Created by shivakumar chirra on 20/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClimateTests/ClimateTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateTests.swift
+//  ClimateTests
+//
+//  Created by shivakumar chirra on 20/05/25.
+//
+
+import Testing
+@testable import Climate
+
+struct ClimateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
